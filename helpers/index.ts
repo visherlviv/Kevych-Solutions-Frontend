3a8d36a1eeb6567377b2a50a629e613e821f353f
Viktor Sherstyuk <slimbaron@Viktors-MacBook-Pro.local>
2023-10-18 21:34:13 +0300

    refactor: fix BASE_URL

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://kevych-solutions-frontend-tyfn-hwnk4tw4s.vercel.app/trains'
+const BASE_URL = 'https://kevych-solutions-frontend-tyfn.vercel.app/trains'
 const getAllTrains = async (): Promise<object[]> => {
     const result = await fetch(BASE_URL, {
         method: 'GET',

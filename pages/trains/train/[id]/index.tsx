4e4f570a82dd6cc2f64025eae8482fc943c52808
Viktor Sherstyuk <slimbaron@Viktors-MacBook-Pro.local>
2023-10-20 08:21:35 +0300

    refactor: remove styles scripts

--- pages/trains/train/[id]/index.tsx
+++ pages/trains/train/[id]/index.tsx
@@ -63,7 +63,6 @@ const Home: React.FC<HomeProps> = ({ project, params }) => {
 
     return (
         <div className="container mx-auto p-4">
-            <script src="https://cdn.tailwindcss.com"></script>
             <h1 className="text-2xl font-bold mb-4">{projectName}</h1>
             <ScheduleDetailsForm onAddTask={addTask} />
             <TaskList schedules={schedules} onDeleteSchedule={deleteTask} />
